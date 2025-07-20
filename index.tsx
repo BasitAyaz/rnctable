9baabb1ed001652d5889238bd33624a2b2e50514
BasitAyaz <basitahmed1997@gmail.com>
2025-07-20 16:13:59 +0500

    added CardView

--- index.tsx
+++ index.tsx
@@ -10,57 +10,68 @@ type propsType = {
     datasource: any[],
     cols: Cols[],
     loading?: boolean,
+    color?: string,
+    cardView?: boolean,
+    displayCard?: (item: any) => any
 }
 
 export default function RNCTable(props: propsType) {
-    const { datasource, cols, loading } = props
+    const { datasource, cols, loading, color, cardView, displayCard } = props
+    const TableStyles = StyleSheet.create({
+        tHead: {
+            backgroundColor: color ?? '#004AAD'
+        },
+        tHeading: {
+            color: 'white',
+            fontWeight: 'bold'
+        },
+        tBody: {
+            backgroundColor: '#f0f0fc',
+            maxHeight: 500
+        },
+        tData: {
+            color: color ?? '#004AAD'
+        },
+        tRow: {
+
+        },
+        flexRow: {
+            flexDirection: "row",
+        },
+        tCell: {
+            padding: 10,
+            width: 100,
+            borderWidth: 1,
+            borderColor: color ?? '#004AAD',
+            flex: 1
+        }
+    })
     return <>
-        {loading ? <Text>Loading...</Text> : <View>
-            <View style={TableStyles.tHead}>
-                <View style={TableStyles.flexRow}>
-                    {cols.map((x, i) => <Text style={[TableStyles.tHeading, TableStyles.tCell]} key={i}>{x.label}</Text>)}
+        {loading ? <Text>Loading...</Text> : datasource && datasource.length === 0 ? <View>
+            <Text style={{ textAlign: 'center', padding: 20 }}>No Rows Available</Text>
+        </View> : <View>
+            {cardView && <View>
+                {datasource.map((item, index) => <View style={{ marginBottom: 5 }} key={index}>
+                    {displayCard && displayCard(item)}
+                </View>)}
+            </View>}
+            {!cardView && <View>
+                <View style={TableStyles.tHead}>
+                    <View style={TableStyles.flexRow}>
+                        {cols.map((x, i) => <Text style={[TableStyles.tHeading, TableStyles.tCell]} key={i}>{x.label}</Text>)}
+                    </View>
                 </View>
-            </View>
-            <View style={TableStyles.tBody}>
-                <ScrollView>
-                    {datasource.map((x, i) => <View
-                        style={TableStyles.flexRow}
-                        key={i}
-                    >
-                        {cols.map((col, ind) => col.displayField ? <View style={TableStyles.tCell} key={ind}>{col.displayField(x)}</View> : <Text style={[TableStyles.tData, TableStyles.tCell]} key={ind}>{x[col.key]}</Text>)}
-                    </View>)}
-                </ScrollView>
-            </View>
+                <View style={TableStyles.tBody}>
+                    <ScrollView>
+                        {datasource.map((x, i) => <View
+                            style={TableStyles.flexRow}
+                            key={i}
+                        >
+                            {cols.map((col, ind) => col.displayField ? <View style={TableStyles.tCell} key={ind}>{col.displayField(x)}</View> : <Text style={[TableStyles.tData, TableStyles.tCell]} key={ind}>{x[col.key]}</Text>)}
+                        </View>)}
+                    </ScrollView>
+                </View>
+            </View>}
         </View >}
     </>
 }
-
-const TableStyles = StyleSheet.create({
-    tHead: {
-        backgroundColor: '#004AAD'
-    },
-    tHeading: {
-        color: 'white',
-        fontWeight: 'bold'
-    },
-    tBody: {
-        backgroundColor: '#f0f0fc',
-        maxHeight: 500
-    },
-    tData: {
-        color: '#004AAD'
-    },
-    tRow: {
-
-    },
-    flexRow: {
-        flexDirection: "row",
-    },
-    tCell: {
-        padding: 15,
-        width: 100,
-        borderWidth: 1,
-        borderColor: '#004AAD',
-        flex: 1
-    }
-})
